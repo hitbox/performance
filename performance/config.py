685819355986e19520cc212e7d5b34c317b199b3
Carl Harris <elgoogemail2007@gmail.com>
2021-05-06 03:07:56 -0400

    dev: type checks for config values

--- performance/config.py
+++ performance/config.py
@@ -1,7 +1,8 @@
 from .exceptions import AppError
 from .utils import exists_and_truthy
 
-_registry = {}
+_required_registry = {}
+_development_registry = {}
 
 class ConfigError(Exception):
     pass
@@ -11,90 +12,89 @@ def isint(value):
     "An integer"
     return isinstance(value, int)
 
-def require(key, validate_func_name):
-    if key in _registry:
+class String:
+    "Is string"
+
+    def __init__(self, min=None):
+        self.min = min
+
+    def __str__(self):
+        return self.__doc__
+
+    def __call__(self, value):
+        return (
+            isinstance(value, str)
+            and (self.min is None or len(value) >= self.min))
+
+
+class List:
+
+    def __init__(self, is_populated=False):
+        self.is_populated = is_populated
+
+    def __call__(self, value):
+        return (
+            isinstance(value, list)
+            and (not self.is_populated or len(value) > 0))
+
+
+class Dictionary:
+
+    def __call__(self, value):
+        return isinstance(value, dict)
+
+
+def require(key, validator, is_development=False):
+    if is_development:
+        registry = _development_registry
+    else:
+        registry = _required_registry
+    if key in registry:
         raise ConfigError('Key already exists, %r' % key)
-    if validate_func_name not in globals():
-        raise ConfigError('Invalid validator name, %r' % validate_func_name)
-    validate_func = globals()[validate_func_name]
-    _registry[key] = validate_func
+    if isinstance(validator, str):
+        # pluck from globals
+        namespace = globals()
+        if validator not in namespace:
+            raise ConfigError('Invalid validator name, %r' % validator)
+        validator = namespace[validator]
+    registry[key] = validator
+
+def require_all(validator, keys, is_development=False):
+    for key in keys:
+        require(key, validator, is_development=is_development)
 
 def raise_for_config(app):
-    for key, validator in _registry.items():
-        try:
-            value = app.config[key]
-        except KeyError:
-            raise ConfigError('Missing config key, %r' % key)
-        else:
-            if not validator(value):
-                raise ConfigError(
-                    'Invalid config value %r for %r. Expected %r'
-                    % (value, key, validator.__doc__))
+    def _raise_for_config(app, registry, prefix=''):
+        for key, validator in registry.items():
+            try:
+                value = app.config[key]
+            except KeyError:
+                raise ConfigError(f'{prefix}Missing config key, {key!r}')
+            else:
+                if not validator(value):
+                    raise ConfigError(
+                        f'{prefix}Invalid config value {value!r} for {key!r}.'
+                        f'Expected {validator}')
+    if app.env == 'development':
+        _raise_for_config(app, _development_registry, prefix='Development: ')
+    _raise_for_config(app, _required_registry)
 
-class ConfigValidator:
-    """
-    Ensure sane configuration as soon as possible.
-    """
-
-    def __init__(
-            self,
-            required_truthy,
-            required_truthy_if_development,
-            required_exists,
-        ):
-        """
-        :param required_truthy: keys required to exist and have a truthy value.
-        :param required_truthy_if_development: keys required truthy if
-                app.env == 'development'.
-        :param required_exists: keys required to exist.
-        """
-        self.required_truthy = required_truthy
-        self.required_truthy_if_development = required_truthy_if_development
-        self.required_exists = required_exists
-
-    def raise_for_config(self, app):
-        """
-        Ensure necessary configuration
-        """
-        for key in self.required_truthy:
-            if not exists_and_truthy(app.config, key):
-                raise AppError(f'{key} must be configured and truthy true.')
-        if app.env == 'development':
-            for key in self.required_truthy_if_development:
-                if not exists_and_truthy(app.config, key):
-                    raise AppError(f'in development, {key} must be configured and'
-                                    ' truthy true.')
-        for key in self.required_exists:
-            if key not in app.config:
-                raise AppError(f'{key} must be configured.')
-
-
-# required to be set to something truthy
-REQUIRED_TRUTHY = [
-    'FIRSTWEEKDAY',
+# universal requirements
+require_all(String(min=1), [
     'SESSION_COOKIE_PATH',
     'REMEMBER_COOKIE_PATH',
-    # used in templates and will blow up without these:
     'DATEFMT',
     'TIMEFMT',
     'DATETIMEFMT',
-    # list of stations to consider for calculating what delays were controllable:
-    'PERFORMANCE_CONTROLLABLE',
-    # <head><title>...
     'PERFORMANCE_HEAD_TITLE',
-    # dict to inject into javascript through the templates:
-    'PERFORMANCE_JAVASCRIPT_INJECTION',
     'PERFORMANCE_REPORT_DATEFMT',
     'PERFORMANCE_REPORT_TITLE',
-]
+])
 
-REQUIRED_TRUTHY_IF_DEVELOPMENT = ['PREFIX']
+require('PERFORMANCE_JAVASCRIPT_INJECTION', Dictionary())
 
-# just required to exist in config
-REQUIRED_EXISTS = []
+require_all(List(is_populated=True), [
+    'PERFORMANCE_CONTROLLABLE',
+])
 
-validate = ConfigValidator(
-    REQUIRED_TRUTHY,
-    REQUIRED_TRUTHY_IF_DEVELOPMENT,
-    REQUIRED_EXISTS
-)
+require('PREFIX', String(min=1), is_development=True)
