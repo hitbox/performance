f4ffb71451b25df21d9422b96ddb71d15e03605d
Carl Harris <elgoogemail2007@gmail.com>
2022-07-26 05:55:11 -0400

    remove ill-advised option

--- performance/extensions.py
+++ performance/extensions.py
@@ -29,15 +29,7 @@ class PrefixMiddleware:
 
 
 assets = fa.Environment()
-db = SQLAlchemy(
-    session_options = dict(
-        # added because creating Delay objects during flight editing causes
-        # detached instances and we get an error when trying to access their
-        # attributes.
-        # https://gist.github.com/krak3n/9fa1268ee0a92a67f71a
-        expire_on_commit = False,
-    ),
-)
+db = SQLAlchemy()
 htmlmin = HTMLMIN()
 login_manager = LoginManager()
 
