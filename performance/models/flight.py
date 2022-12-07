590175e7ba14279aeb89d7174a1543c48ae889a4
Carl Harris <elgoogemail2007@gmail.com>
2022-12-07 15:34:38 -0500

    remove unused import

--- performance/models/flight.py
+++ performance/models/flight.py
@@ -12,7 +12,6 @@ from .delay import Delay
 from .flight_delay import DestinationDelay
 from .flight_delay import OriginDelay
 from .flight_mixin import FlightMixin
-from .flight_type import FlightType
 from .flight_type import FlightTypeRelationshipMixin
 from .mixin import MetaMixin
 
