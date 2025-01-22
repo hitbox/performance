646e3be422c936b2803eb2940e64719acab55d44
Carl Harris <hitbox@ryzen.com>
2025-01-22 07:41:13 -0500

    Refactor app.py into descriptive functions.

--- performance/app.py
+++ performance/app.py
@@ -13,14 +13,7 @@ from . import shell
 from . import utils
 from . import views
 
-def create_app(silent_config=False):
-    """
-    Performance report Flask app.
-    """
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_envvar('PERFORMANCE_CONFIG', silent=silent_config)
-    config.raise_for_config(app)
-
+def setdefault_config(app):
     app.config.setdefault('LATE_GT', 0)
     app.config.setdefault('EARLY_LT', 0)
     # if diff minutes do not meet threshold, always show these delay codes
@@ -33,6 +26,31 @@ def create_app(silent_config=False):
     performance_stats.setdefault('delays_gt_30_count', True)
     performance_stats.setdefault('on_time_performance_gt_15', True)
     performance_stats.setdefault('on_time_performance_gt_30', True)
+
+def ensure_metadata(app):
+    # ensure that at least one object exists for metadata settings
+    db = extensions.db
+    with app.app_context():
+        stmt = db.select(models.Performance)
+        settings = db.session.scalars(stmt).one_or_none()
+        if not settings:
+            db.session.add(models.Performance())
+            db.session.commit()
+
+def init_app(app):
+    """
+    Initialize other things that need to initialize against the app.
+    """
+    commands.init_app(app)
+    converters.init_app(app)
+    extensions.init_app(app)
+    shell.init_app(app)
+    views.init_app(app)
+
+def init_context_processor(app):
+    """
+    Add an application wide context for templates.
+    """
 
     @app.context_processor
     def context_processor():
@@ -47,18 +65,17 @@ def create_app(silent_config=False):
         )
         return context
 
-    commands.init_app(app)
-    converters.init_app(app)
-    extensions.init_app(app)
-    shell.init_app(app)
-    views.init_app(app)
+def create_app(silent_config=False):
+    """
+    Performance report Flask app.
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_envvar('PERFORMANCE_CONFIG', silent=silent_config)
+    config.raise_for_config(app)
+    setdefault_config(app)
 
-    # ensure that at least one object exists for metadata settings
-    db = extensions.db
-    with app.app_context():
-        settings = db.session.scalars(db.select(models.Performance)).one_or_none()
-        if not settings:
-            db.session.add(models.Performance())
-            db.session.commit()
+    init_context_processor(app)
+    init_app(app)
+    ensure_metadata(app)
 
     return app
