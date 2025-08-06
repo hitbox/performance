06f4d4e3f199845bfd467e7f75e2d29628d74cb9
Carl L. Harris <Carl.Harris@airborneglobal.com>
2025-08-06 13:43:13 -0400

    Added database uri fallback

---
+++ performance/database.py
@@ -0,0 +1,14 @@
+import sqlalchemy as sa
+
+def first_working_url(url_list):
+    """
+    Return the first url that connects.
+    """
+    for url in url_list:
+        try:
+            engine = sa.create_engine(url)
+            with engine.connect() as connection:
+                pass
+            return url
+        except Exception as e:
+            pass
