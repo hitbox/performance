cadccc25dd4ebf18f97f43f23c6536acb9a05f77
Carl Harris <hitbox@ryzen.com>
2026-09-22 15:51:34 -0400

    Add prefix middleware back to wsgi create app func

--- performance/wsgi.py
+++ performance/wsgi.py
@@ -2,17 +2,9 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.wrappers import Response
 
 from .app import create_app
+from .app import init_prefix_middleware
 
 def create_wsgi_app():
     app = create_app()
-
-    if 'PREFIX' in app.config:
-        # Overwrite app name with dispatch middleware for prefixing the url
-        prefix = app.config['PREFIX']
-        print(f'Using prefix: {prefix}')
-        app = DispatcherMiddleware(
-            Response('Not Found', status=404), {
-                prefix: app
-        })
-
+    app = init_prefix_middleware(app)
     return app
